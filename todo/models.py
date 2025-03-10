de22da5b4d5232755a595bde62ebabda59063c2f
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-03-10 21:40:20 +0100

    Initialisation du projet Django avec l'application Todo
    - Ajout du fichier .gitignore pour ignorer les fichiers Python et Django
    - Configuration du projet Django avec les fichiers de base
    - Création de l'application Todo avec des modèles pour les tâches, commentaires et photos
    - Configuration des paramètres du projet et des URLs

---
+++ todo/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TodoItemComment(models.Model):
+    todo_item = models.ForeignKey(TodoItem, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+
+
+class TodoPhoto(models.Model):
+    todo_item = models.ForeignKey(TodoItem, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='todo_photos/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.photo.url
+
